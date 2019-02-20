2e3f0b55f0afc856d8175bf6506ad007b2c15b18
Simon Viet <simonviet10@gmail.com>
2019-02-20 10:14:00 +0100

    Initial commit

---
+++ src/ch/epfl/javass/ch/epfl/javass/bits/Bits32.java
@@ -0,0 +1,36 @@
+package ch.epfl.javass.ch.epfl.javass.bits;
+
+public class Bits32 {
+    private Bits32 () {}
+
+    private void paire_valide(int value,int size){
+        if ( (size < 0 ) || (size > 32))
+
+    }
+
+
+
+    public static int mask (int start, int size) {
+
+        return 0;}
+
+
+    public static  int extract (int bits, int start, int size) {
+        return 0;
+    }
+
+
+    public static int pack(int v1, int s1, int v2, int s2) {
+
+        return 0 ;
+    }
+
+    public static int pack(int v1, int s1, int v2, int s2,int v3, int s3) {
+
+        return 0 ;}
+
+
+    public static int pack(int v1, int s1, int v2, int s2,int v3, int s3,int v4, int s4, int v5, int s5, int v6, int s6, int v7, int s7) {
+
+        return 0 ;}
+}
---
+++ src/ch/epfl/javass/preconditions.java
@@ -0,0 +1,14 @@
+package ch.epfl.javass;
+
+public class preconditions {
+
+        private preconditions(){} //constructeur privé pour rendre impossible la creation d'instances de classes (on a besoin que des fonctions)
+
+        public static void checkArgument (boolean b) {}
+
+        public static int checkIndex (int index, int size) {return 0;}
+
+
+
+
+}
